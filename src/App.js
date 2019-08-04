b7b80de6ab5034d207d9f64abfb0ab3ff75ab384
tinyvices <tinyvices@tinyvices.io>
2019-08-04 11:30:19 -0400

    https

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
         </p>
-<iframe src="http://free.timeanddate.com/countdown/i6jyt1kq/n419/cf100/cm0/cu3/ct0/cs1/cacfff/cr0/ss0/cacfff/cpcfff/pc222/tc333/fs100/tatMOVE.%20IN.%20DAY./tacfff/tptMOVE.%20IN.%20DAY./tpcfff/matSeptember%2020%2C%202019/macfff/mptSeptember%2020%2C%202019/mpcfff/iso2019-09-20T00:00:00/pa0" allowTransparency="true" frameborder="0" width="320" height="125"></iframe>
+	    <iframe src="https://free.timeanddate.com/countdown/i6jyt1kq/n419/cf100/cm0/cu3/ct0/cs1/cacfff/cr0/ss0/cacfff/cpcfff/pc222/tc333/fs100/szw320/szh135/tatMOVE.%20IN.%20DAY./tacfff/tptMOVE.%20IN.%20DAY./tpcfff/matSeptember%2020%2C%202019/macfff/mptSeptember%2020%2C%202019/mpcfff/iso2019-09-20T00:00:00/pa0" allowTransparency="true" frameborder="0" width="320" height="135"></iframe>
 
 	  </div>
     );
